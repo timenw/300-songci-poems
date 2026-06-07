cda188353fd7cd6e297020285068a835ce6d6f50
timenw <timenw@github.com>
2026-06-07 07:49:19 +0000

    Add ExperimentalMaterial3Api opt-in to fix Card(onClick) experimental API errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,10 @@ android {
 
     kotlinOptions {
         jvmTarget = "17"
-        freeCompilerArgs += listOf("-opt-in=androidx.compose.ui.ExperimentalComposeUiApi")
+        freeCompilerArgs += listOf(
+            "-opt-in=androidx.compose.ui.ExperimentalComposeUiApi",
+            "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api"
+        )
     }
 
     buildFeatures {
